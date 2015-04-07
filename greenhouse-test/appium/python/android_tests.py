6967035f519bf15ed05d32d2f1f8825f224afd3f
Priit Lätt <priit.latt@lab.mobi>
2015-04-07 15:07:06 +0300

    Add Appium Python tests

---
+++ greenhouse-test/appium/python/android_tests.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+
+from appium import webdriver
+
+# Returns abs path relative to this file and not cwd
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+
+class AndroidTests(unittest.TestCase):
+    def setUp(self):
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '4.2'
+        desired_caps['deviceName'] = 'Android Emulator'
+        desired_caps['app'] = PATH('../../App/build/outputs/apk/App-greenhouseandroid-debug.apk')
+
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+    def tearDown(self):
+        # end the session
+        self.driver.quit()
+
+
+    def test_activity(self):
+        self.assertIsNotNone(self.driver.current_activity)
+
+    def test_assert_true(self):
+        self.assertTrue(True)
+
+    def test_assert_false(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(SimpleAndroidTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
