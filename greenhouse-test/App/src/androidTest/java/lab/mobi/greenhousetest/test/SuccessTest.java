c31a1ae157d2e684aca31d65afeb640d1fd161f2
lauralilleste <lauralilleste@gmail.com>
2017-10-13 16:15:54 +0300

    aa

--- greenhouse-test/App/src/androidTest/java/lab/mobi/greenhousetest/test/SuccessTest.java
+++ greenhouse-test/App/src/androidTest/java/lab/mobi/greenhousetest/test/SuccessTest.java
@@ -106,8 +106,8 @@ public class SuccessTest extends AndroidTestCase {
     
     @Ignore("Test is ignored as a demonstration")
     @Test
-    public void testSame() {
-        assertThat(1, is(1));
+    public void testIgnore() {
+        assertEquals(1, 1);
     }
     
 }
